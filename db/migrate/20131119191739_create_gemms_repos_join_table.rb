98ac9beba19603e3fb7c8ed5d2fa7117a1f6ba59
Amal Hussein <amalhussein08@gmail.com>
2013-11-19 14:26:06 -0500

    creating the repos-gemms join table

---
+++ db/migrate/20131119191739_create_gemms_repos_join_table.rb
@@ -0,0 +1,11 @@
+class CreateGemmsReposJoinTable < ActiveRecord::Migration
+  def change
+  	create_table :gemms_repos do |t| 
+	  	t.belongs_to :gemm
+			t.belongs_to :repo
+			end
+			add_index :gemms_repos, :gemm_id
+			add_index :gemms_repos, :repo_id
+			add_index :gemms_repos, [:gemm_id, :repo_id]
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131119182726) do
+ActiveRecord::Schema.define(version: 20131119191739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 20131119182726) do
     t.text     "bug_tracker_uri"
     t.string   "platform"
   end
+
+  create_table "gemms_repos", force: true do |t|
+    t.integer "gemm_id"
+    t.integer "repo_id"
+  end
+
+  add_index "gemms_repos", ["gemm_id", "repo_id"], name: "index_gemms_repos_on_gemm_id_and_repo_id", using: :btree
+  add_index "gemms_repos", ["gemm_id"], name: "index_gemms_repos_on_gemm_id", using: :btree
+  add_index "gemms_repos", ["repo_id"], name: "index_gemms_repos_on_repo_id", using: :btree
 
   create_table "repos", force: true do |t|
     t.string   "name"

b39c7c23e8c3c1d24f4ed107ba19908d9c71eb43
Amal Hussein <amalhussein08@gmail.com>
2013-11-19 22:43:54 -0500

    added gem parse methods to repo model

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,3 +1,37 @@
 class Repo < ActiveRecord::Base
 		has_and_belongs_to_many :gemms
+		include HTTParty
+	# http://edgeguides.rubyonrails.org/active_record_callbacks.html
+	before_validation :parse_and_store_gemms
+
+	# Overloading the as_json method
+	# to include gemms in the json object returned
+	# as_json is automatically called when you return json
+	def as_json(options={})
+		super(options.merge(include: :gemms))
+	end
+
+	private # can only be called from within the model
+
+	def parse_and_store_gemms
+    gemm_list = mine_gems
+		gemm_list.each do |gemm|
+			self.gemms << Gemm.find_or_create_by(name: gemm)
+		end
+	end
+
+  def mine_gems
+  	binding.pry
+    url = "http://raw.github.com/#{repo_username}/#{repo_name}/master/Gemfile"
+    response = HTTParty.get(url)
+    gemms = []
+    begin
+      response.each_line do |line|
+        gemms << $1 if line =~ ( /gem\s\'(.*?)\'/ || /gem\s\"(.*?)\"/ )
+      end
+    rescue => err
+      puts "Exception: #{err}"
+    end
+    return gemms
+  end
 end

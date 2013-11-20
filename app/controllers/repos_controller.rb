724ce0b9960aa1cf57789347c8f93580d924390b
Amal Hussein <amalhussein08@gmail.com>
2013-11-19 23:26:40 -0500

    setup repos controller

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,2 +1,12 @@
 class ReposController < ApplicationController
+
+	def show
+		@resoure = Resource.new 
+		@repo = Repo.find_or_create_by(username: params[:github_user], name: params[:github_repo])
+		respond to do |format|
+			format.hmtl { @repo }
+			format.js 
+		end 
+	end
+
 end
